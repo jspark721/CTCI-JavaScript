9387689ba987207f3a31fc4de9b6a7dc6787c0fe
Julie Park <jspark721@gmail.com>
2018-09-13 12:17:21 -0700

    finish 1.5 one away

---
+++ chapter01/1.5-oneAway.js
@@ -0,0 +1,43 @@
+const oneAway = function(str1,str2) {
+  //if we insert a character, then s1's current char should match s2's next char
+  //if remove, then s1's next char should match s2's current char
+  //if replaced, then s1's next char should match s2's next char
+  // max one edit
+  let edit = 1;
+  let maxLength = Math.max(str1.length, str2.length);
+  let diff = Math.abs(str1.length - str2.length); // get absolute value because we don't know if str1 or str2 is longer
+
+  //if length is different and greater than max edit, return false
+  if (diff > edit) {
+    return false;
+  }
+  //iterate through strings at the same time, checking for difference
+  //store max length for forloop condition
+  //have two indices - continue iterating until the greater of the two strings have finished
+  for(let i = 0, j = 0; i < maxLength || j < maxLength; i++, j++) {
+    let char1 = str1[i]; // i corresponds to indices in str1
+    let char2 = str2[j]; // j corresponds to indices in str2
+    //if difference is found, decrement the number of edits, then check if edit is below zero, then return false
+    if(char1 !== char2) { //replace char
+      edit--;
+      if(edit < 0) {
+        return false;
+      }
+      if(char1 === str2[j + 1]) { //insert char
+        j++;
+      } else if(str1[i+1] === char2) {//remove char
+        i++;
+      }
+    }
+  }
+  //when forloop is done, all has passed - return true
+  return true;
+}
+
+//test
+console.log(
+  oneAway('cake', 'caked'),
+  oneAway('pale', 'pa'),
+  oneAway('hello','mellow'),
+  oneAway('bake', 'cake')
+)
