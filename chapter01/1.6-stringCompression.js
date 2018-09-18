1f6e5fe52a3f5a1c63d57b400f6948d14a861354
Julie Park <jspark721@gmail.com>
2018-09-18 11:06:11 -0700

    1.6 string compression algorithm

---
+++ chapter01/1.6-stringCompression.js
@@ -0,0 +1,30 @@
+//compress the string if there is a repeated character - put the number in (ex: aabcccaa would return a2b1c3a2)
+
+//string only has uppercase and lowercase letters (a-z)
+
+//input: string
+//output: string
+
+const compress = function(str) {
+  let compressedStr = '';
+  let charCount = 1;
+
+  for(let i = 0; i < str.length; i++) {
+    if(str[i] === str[i+1]) {
+      charCount++;
+    } else {
+      compressedStr += str[i] + charCount;
+      charCount = 1;
+    }
+  }
+  if (compressedStr.length >= str.length) {
+    return str;
+  }
+  return compressedStr;
+}
+
+//test
+console.log(compress('aaAAaa'));
+console.log(compress('aabcccccaaa'));
+console.log(compress('bbbrrrrb'));
+console.log(compress('heeeeyyyyyy'));
