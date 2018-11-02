06df6de4ff2df540a56b63ecdb96dda59a7a5aa6
Julie Park <jspark721@gmail.com>
2018-11-02 16:44:57 -0700

    rotate matrix notes

--- chapter01/1.7-rotateMatrix.js
+++ chapter01/1.7-rotateMatrix.js
@@ -1,1 +1,22 @@
 //given an image represented by an NxN matrix, where each pixel in the image is 4 bytes, write a method to rotate the image by 90 degrees.
+
+//input: array of arrays (nxn matrix)
+//output: rotated matrix - 90 deg clockwise
+
+/* Input: [
+  [1, 2, 3],
+  [4, 5, 6],
+  [7, 8, 9]
+];
+
+Output: [
+  [7, 4, 1],
+  [8, 5, 2],
+  [9, 6, 3]
+];
+
+matrix[row][col]
+
+for i = each row
+for j = each column
+*/
