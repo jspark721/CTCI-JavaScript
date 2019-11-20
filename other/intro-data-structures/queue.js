9ea0f7c11682271dda4c2cd1afed199500c13f80
jspark721 <jspark721@gmail.com>
2019-11-19 17:08:21 -0800

    update queue

--- other/intro-data-structures/queue.js
+++ other/intro-data-structures/queue.js
@@ -7,16 +7,20 @@ class Queue {
 
     // enqueues a new value at the end of the queue
     enqueue(value) {
-        this._storage[this._length] = value;
+        this._storage[this._length + this._headIndex] = value;
         this._length++;
     }
     
     // dequeues the value from the beginning of the queue and returns it
     dequeue() {
-
+        const firstValue = this._storage[this._headIndex];
+        delete this._storage[this._headIndex];
+        this._length--;
+        this._headIndex++;
+        return firstValue;
     }
 
-    // returns the value at the beginning of the queue without removing it from the queue
+    // returns the value at the beginning of the queue without removing it from the queue 
     peek() {
 
     }
@@ -24,12 +28,12 @@ class Queue {
 
 let myQ = new Queue();
 
-myQ.enqueue('front');
-myQ.enqueue('back');
+myQ.enqueue('zero');
+myQ.enqueue('one');
 // {_storage: {0: 'front, 1: 'back'}} // length 2
 // headIndex: 0
 
-// myQ.dequeue();
+myQ.dequeue();
 // // {_storage: {1: 'back'}} // length 1
 // // headIndex: 1
 
