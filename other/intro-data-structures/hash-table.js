595ef9a557445ff37aee849feb838e79e92a449d
jspark721 <jspark721@gmail.com>
2019-11-21 12:36:22 -0800

    hash table notes

--- other/intro-data-structures/hash-table.js
+++ other/intro-data-structures/hash-table.js
@@ -65,3 +65,61 @@ when you're thinking about optimizing a problem, when looking for a fast lookup,
 try to always use ES6 so you can show that you learn and grow as technology changes
 
 */
+
+class HashTable2 {
+  constructor(val) {
+    this._storage = [];
+    this._tableSize = val;
+  }
+
+  //inserts a new key-value pair
+  insert(key, value) {
+    const index = this._hash(key, this._tableSize);
+
+    if(!this._storage[index]) {
+      this._storage[index] = [];
+      // [0, 0, 0, []] // this is handling a collision
+    }
+    this._storage[index].push([key,value]);
+    // adding the key value pairs 
+    // [0, 0, 0, [['a', 1], ['b', 2]]]
+  }
+
+  //deletes a key-value pair
+  remove() {
+
+  }
+
+  //returns the value associated with a key 
+  retrieve(key) {
+    const index = this._hash(key, this._tableSize);
+    const arrayAtIndex = this._storage[index];
+
+    if(arrayAtIndex) {
+      for(let i = 0; i < arrayAtIndex.length; i++) {
+        const keyValueArray = arrayAtIndex[i];
+        if(keyValueArray[0] === key) {
+          return keyValueArray[1];
+        }
+      }
+    }
+  }
+
+  // hashes string value into an integer that can be mapped to an array index
+  _hash(str, n) {
+    let sum = 0;
+    for(let i = 0; i < str.length; i++) {
+      sum += str.charCodeAt(i) * 3;
+    }
+    return sum % n;
+  }
+}
+
+const myHT = new HashTable2(10);
+
+console.log(myHT);
+
+myHT.insert('a', 1);
+myHT.insert('b', 2);
+
+console.log(myHT);
