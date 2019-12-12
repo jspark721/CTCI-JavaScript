867d9f5df6d46a10ab384139aa003d3b36364163
jspark721 <jspark721@gmail.com>
2019-12-11 16:22:20 -0800

    edit folder name and add graph

---
+++ other/data-structures/graphs.js
@@ -0,0 +1,10 @@
+/* 
+Graphs are non-linear data structures 
+a graph is a system in which there are potentially multiple ways to get from an arbitrary point, A, to another arbitrary point, B
+
+A graph is normally defined as a pair of sets (V,E). V is a set of arbitrary objects called vertices or nodes, and E is a set of pairs of vertices, which we call edges or (more rarely, arcs)
+
+graph traversal (or graph search) refers to the process of visiting (checking and/or updating) each vertex in a graph
+
+to visit each node or vertext which is a connected component, tree based algorithms are used -- you can do this easily by iterating through all the vertices of the graph, performing the algorithms on each vertex that is still unvisted when examined
+*/
