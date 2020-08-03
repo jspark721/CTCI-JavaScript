133eb3a5b2bfcfd98622eeacb5493034aed3a08a
Julie Park <jspark721@gmail.com>
2020-08-03 01:31:18 -0700

    solve remove element - lc easy

---
+++ leetcode/27-removeElement.js
@@ -0,0 +1,19 @@
+/**
+ * @param {number[]} nums
+ * @param {number} val
+ * @return {number}
+ */
+var removeElement = function (nums, val) {
+  if (nums.length === 0) {
+    return nums;
+  }
+
+  for (let i = 0; i < nums.length; i++) {
+    if (nums[i] === val) {
+      nums.splice(i, 1);
+      i--;
+    }
+  }
+
+  return nums.length;
+};
