3fb05a033d8da32074b81c557c89d45eba21b48b
Julie Park <jspark721@gmail.com>
2020-10-19 23:38:35 -0700

    solve array problem first bad version

---
+++ leetcode/278-firstBadVersion.js
@@ -0,0 +1,53 @@
+/*
+You are a product manager and currently leading a team to develop a new product. Unfortunately, the latest version of your product fails the quality check. Since each version is developed based on the previous version, all the versions after a bad version are also bad.
+
+Suppose you have n versions [1, 2, ..., n] and you want to find out the first bad one, which causes all the following ones to be bad.
+
+You are given an API bool isBadVersion(version) which returns whether version is bad. Implement a function to find the first bad version. You should minimize the number of calls to the API.
+
+Input: n = 5, bad = 4
+Output: 4
+Explanation:
+call isBadVersion(3) -> false
+call isBadVersion(5) -> true
+call isBadVersion(4) -> true
+Then 4 is the first bad version.
+
+basically a search through the array to find the "first bad version" where isBadVersion returns true
+
+we can do a linear search but since it's sorted we can do a binary search which would take O(log n) 
+*/
+
+/**
+ * Definition for isBadVersion()
+ *
+ * @param {integer} version number
+ * @return {boolean} whether the version is bad
+ * isBadVersion = function(version) {
+ *     ...
+ * };
+ */
+
+/**
+ * @param {function} isBadVersion()
+ * @return {function}
+ */
+var solution = function (isBadVersion) {
+  /**
+   * @param {integer} n Total versions
+   * @return {integer} The first bad version
+   */
+  return function (n) {
+    let start = 0;
+    let end = n;
+    while (start < end) {
+      let mid = Math.floor((start + end) / 2);
+      if (isBadVersion(mid)) {
+        end = mid;
+      } else {
+        start = mid + 1;
+      }
+    }
+    return start;
+  };
+};
