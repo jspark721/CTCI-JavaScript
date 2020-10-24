8f51251a9322c8a075da56a070dbb7c98fcaf16f
Julie Park <jspark721@gmail.com>
2020-10-23 23:59:33 -0700

    permutation in string problem

---
+++ ic/problems/dynammic-programming/stringPermutation.js
@@ -0,0 +1,17 @@
+/*
+Write a recursive function for generating all permutations of an input string. Return them as a set.
+
+Don't worry about time or space complexity—if we wanted efficiency we'd write an iterative version.
+
+To start, assume every character in the input string is unique.
+
+Your function can have loops—it just needs to also be recursive.
+
+input: 'ABC'
+output: Set {'ABC', 'ACB', 'BAC', 'BCA', 'CBA','CAB'}
+
+*/
+
+const permute = (string) => {
+  //base case
+};
